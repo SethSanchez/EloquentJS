d455cde865e9d85c6057a2cbd9a1b0db24d4e946
Seth Sanchez <theoneghost@gmail.com>
2016-09-22 16:05:40 -0500

    Completed Ch4 Ex2

--- reverse_array.js
+++ reverse_array.js
@@ -4,7 +4,7 @@
 
 function reverseArray(array) {
     var position = array.length - 1;
-    var copyMe = array[position];
+    //var copyMe = array[position];
     var newArray = [];
 
     if (Array.isArray(array) == false) {
@@ -20,4 +20,22 @@ function reverseArray(array) {
     return newArray;
 }
 
-reverseArray([1,3,2,6])
+//Pretty sure I missed the point on this one, but it does what it asks for.
+function reverseArrayInPlace(array) {
+    var origArray = [];
+    var position = array.length -1;
+
+    if (Array.isArray(array) == false) {
+        console.log("I need a real array bro.");
+    } else {
+        console.log("I'm reversing: [" + array + "]");
+        for (var length = 1; length <= array.length; length++) {
+            origArray.push(array[position]);
+           position -= 1;
+        }
+    }
+    array = origArray;
+    return array;
+}
+
+reverseArray(reverseArrayInPlace([1,1,3,4,2,0,6]))
