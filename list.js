767b9e33e375028a6fbd7b40ec931d4fea25eb2c
Seth Sanchez <theoneghost@gmail.com>
2016-09-22 16:17:51 -0500

    Ch4 Ex3 initial commit

---
+++ list.js
@@ -0,0 +1,7 @@
+//Ch4 Ex3
+//3.0 Write function arrayToList that builds a nested list from a given array.
+//3.1 Write function listToArray that turns an list into an array
+//3.2 Write helper function prepend which takes an element and a list and creates a new list that adds the element to the front of the input list
+//3.3 White helper function nth (recursively) which take a list and a number, and returns the element at that position in the list, or undefined otherwise.
+
+//fml
